9a78c4d02dec3ac88de377b1957c152f77cd8c39
T9mik <71222557+T9mik@users.noreply.github.com>
2023-01-11 20:27:09 +0100

    well ...

--- PriradUdaje.cpp
+++ PriradUdaje.cpp
@@ -18,7 +18,7 @@ void PriradUdaje::priradKolajeKStanici()
 			if (nacitanieMienStanic.getPrvyUdajInt(i) == nacitajStanice.getPrvyUdajInt(x))
 			{
 				Stanica stanica(nacitanieMienStanic.getDruhyUdajString(i), nacitanieMienStanic.getPrvyUdajInt(i));
-				stanica.priradStaniciKolaje();
+				stanica.priradStaniciKolaje(nacitajStanice.getDruhyUdajString(x));
 				stanice.push_back(stanica);
 				stanica.vypisStanicu();
 			}
